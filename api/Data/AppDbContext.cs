a59d0b08f6dbb9f94f4dc2fad3f93791f425c35a
Sergio <Sergio@DESKTOP-32OC9OI>
2023-09-04 22:44:52 +0200

    Add project files.

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Pokemon> Pokemon
+    }
+}
---
+++ api/Models/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class Pokemon
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Identifier { get; set; }
+        public int Species_id { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public int Base_experience { get; set; }
+        public int Order { get; set; }
+        public int Is_default { get; set; }
+    }
+}
