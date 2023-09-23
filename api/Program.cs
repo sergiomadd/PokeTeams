9d457ee3944a4fffc391d5f07e9375afa27f62bf
Sergio <sergiocortmon@gmail.com>
2023-09-23 14:48:42 +0200

    feat(api): add config for lowercase endpoints

--- api/Program.cs
+++ api/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IPokemonService, PokemonService>();
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
