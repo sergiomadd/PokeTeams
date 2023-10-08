469060452dba39e10cbfc440e77941338828f638
Sergio <sergiocortmon@gmail.com>
2023-10-09 01:44:50 +0200

    feat(ui): improve pokemon model

--- ui/src/app/models/pokemon.model.ts
+++ ui/src/app/models/pokemon.model.ts
@@ -1,19 +1,44 @@
+import { Ability } from "./ability.model"
+import { Item } from "./item.model"
+import { Move } from "./move.model"
+import { Nature } from "./nature.model"
+
+
 export interface Pokemon 
 {
 	name: string,
-	nickname: string,
-	number: number,
-	types: string[],
-	teraType: string,
-	item: string,
-	ability: string,
-	nature: string,
-	moves: string[],
-	stats: string[],
-	ivs: string[],
-	evs: string[],
-	level: number,
-	shiny: false,
-	gender: string,
+	nickname?: string,
+	dexNumber: number,
+	types: 
+		{
+			id: number,
+			type: string
+		}[],
+	teraType?: string,
+	item?: Item,
+	ability?: Ability,
+	nature?: Nature,
+	moves?: Move[],
+	stats: 
+	{
+		identifier: string,
+		name: string,
+		stat: number
+	}[],
+	ivs?: 
+	{
+		identifier: string,
+		name: string,
+		stat: number
+	}[],
+	evs?: 
+	{
+		identifier: string,
+		name: string,
+		stat: number
+	}[],
+	level?: number,
+	shiny?: false,
+	gender?: string,
 }
   
