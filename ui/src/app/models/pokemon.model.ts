500dca59b601f55296e4967760b6745c83ad33cf
Sergio <sergiocortmon@gmail.com>
2023-10-10 14:24:59 +0200

    feat(ui): change stats in pokemon model for its own type

--- ui/src/app/models/pokemon.model.ts
+++ ui/src/app/models/pokemon.model.ts
@@ -2,6 +2,7 @@ import { Ability } from "./ability.model"
 import { Item } from "./item.model"
 import { Move } from "./move.model"
 import { Nature } from "./nature.model"
+import { Stat } from "./stat.model"
 
 
 export interface Pokemon 
@@ -19,24 +20,9 @@ export interface Pokemon
 	ability?: Ability,
 	nature?: Nature,
 	moves?: Move[],
-	stats: 
-	{
-		identifier: string,
-		name: string,
-		stat: number
-	}[],
-	ivs?: 
-	{
-		identifier: string,
-		name: string,
-		stat: number
-	}[],
-	evs?: 
-	{
-		identifier: string,
-		name: string,
-		stat: number
-	}[],
+	stats: Stat[],
+	ivs?: Stat[],
+	evs?: Stat[],
 	level?: number,
 	shiny?: false,
 	gender?: string,
---
+++ ui/src/app/models/stat.model.ts
@@ -0,0 +1,6 @@
+export interface Stat
+{
+  identifier: string,
+  name: string,
+  stat: number
+}
