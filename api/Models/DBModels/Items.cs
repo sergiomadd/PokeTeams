d551f78069dcb296a474c5a121e633bb5384b5c0
Sergio <sergiocortmon@gmail.com>
2023-10-14 16:45:14 +0200

    fix(api): add identifier to item model for getting item image

---
+++ api/Models/DBModels/Items.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Models.DBModels
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Identifier { get; set; }
+        public Items(int id, string identifier)
+        {
+            Id = id;
+            Identifier = identifier;
+        }
+    }
+}
--- api/Models/Item.cs
+++ api/Models/Item.cs
@@ -2,11 +2,13 @@
 {
     public class Item
     {
+        public string Identifier { get; set; }
         public string Name { get; set; }
         public string Prose { get; set; }
 
-        public Item(string name, string prose)
+        public Item(string identifier, string name, string prose)
         {
+            Identifier = identifier;
             Name = name;
             Prose = prose;
         }
