fda11ae8ccc211e495f45d786612682c9a59e3e5
Sergio <sergiocortmon@gmail.com>
2023-11-17 14:28:32 +0100

    feat(ui): add pokemon colors files

---
+++ ui/src/app/styles/pokemonColors.ts
@@ -0,0 +1,22 @@
+export enum Colors
+{
+    //Type colors
+    normal = "#A8A77A",
+    fire = "#EE8130",
+    water = "#6390F0",
+    electric = "#F7D02C",
+    grass = "#7AC74C",
+    ice = "#96d9d6",
+    fighting = "#C22E28",
+    poison = "#A33EA1",
+    ground = "#e2bf65",
+    flying = "#A98FF3",
+    psychic = "#F95587",
+    bug = "#A6B91A",
+    rock = "#B6A136",
+    ghost = "#735797",
+    dragon = "#6F35FC",
+    dark = "#705746",
+    steel = "#B7B7CE",
+    fairy = "#D685AD"
+}
