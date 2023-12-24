40d52b17f68fb660569762c9032ad1018e15d861
Sergio <sergiocortmon@gmail.com>
2023-12-24 20:10:53 +0100

    feat(ui): add temporary json to camelCase converter

--- ui/src/app/services/util.ts
+++ ui/src/app/services/util.ts
@@ -8,3 +8,28 @@ export function getErrorMessage(error: unknown)
   } 
   return error;
 }
+
+export function toCamelCase(o) {
+  var newO, origKey, newKey, value
+  if (o instanceof Array) {
+    return o.map(function(value) {
+        if (typeof value === "object") {
+          value = toCamelCase(value)
+        }
+        return value
+    })
+  } else {
+    newO = {}
+    for (origKey in o) {
+      if (o.hasOwnProperty(origKey)) {
+        newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString()
+        value = o[origKey]
+        if (value instanceof Array || (value !== null && value.constructor === Object)) {
+          value = toCamelCase(value)
+        }
+        newO[newKey] = value
+      }
+    }
+  }
+  return newO
+}
