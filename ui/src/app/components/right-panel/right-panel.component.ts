a14e1364ee21fcaa5d412d6b56fe0588aeb8233a
Sergio <sergiocortmon@gmail.com>
2024-02-07 16:55:40 +0100

    Commit for angular updating

--- ui/src/app/components/right-panel/right-panel.component.ts
+++ ui/src/app/components/right-panel/right-panel.component.ts
@@ -20,29 +20,4 @@ export class RightPanelComponent
     this.user = await this.userService.getLoggedUser();
   }
 
-  async logOut()
-  {
-    await this.userService.logOut()
-    localStorage.removeItem(".AspNetCore.Identity.Application");
-    this.clearCookies();
-    this.user = null;
-  }
-
-  clearCookies()
-  {
-    console.log("local storage", localStorage)
-    console.log("cookie", this.getCookie(".AspNetCore.Identity.Application"))
-    console.log("doc", document.cookie)
-    localStorage.removeItem(".AspNetCore.Identity.Application");
-    this.delete_cookie(".AspNetCore.Identity.Application");
-  }
-
-  getCookie(name) {
-    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
-    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
-    return match ? match[1] : null;
-}
-delete_cookie(name) {
-  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
 }
--- ui/src/app/components/user-options/user-options.component.ts
+++ ui/src/app/components/user-options/user-options.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-options',
@@ -8,15 +9,38 @@ import { User } from 'src/app/models/user.model';
 })
 export class UserOptionsComponent 
 {
-  user?: User;
-
-  logOut()
-  {
-    
-  }
+  userService = inject(UserService);
+  user?: User | null;
 
   deleteAccount()
   {
     
   }
+
+  
+  async logOut()
+  {
+    await this.userService.logOut()
+    localStorage.removeItem(".AspNetCore.Identity.Application");
+    this.clearCookies();
+    this.user = null;
+  }
+
+  clearCookies()
+  {
+    console.log("local storage", localStorage)
+    console.log("cookie", this.getCookie(".AspNetCore.Identity.Application"))
+    console.log("doc", document.cookie)
+    localStorage.removeItem(".AspNetCore.Identity.Application");
+    this.delete_cookie(".AspNetCore.Identity.Application");
+  }
+
+  getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+delete_cookie(name) {
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
 }
