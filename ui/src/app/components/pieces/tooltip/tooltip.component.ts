b93cd559fecf91dc26917bd80150431647a1431d
Sergio <sergiocortmon@gmail.com>
2024-05-26 20:01:15 +0200

    feat(ui): add tooltip component

---
+++ ui/src/app/components/pieces/tooltip/tooltip.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tooltip',
+  templateUrl: './tooltip.component.html',
+  styleUrl: './tooltip.component.scss'
+})
+export class TooltipComponent 
+{
+  @Input() text?: string;
+  @Input() side: string = "left";  
+
+
+}
