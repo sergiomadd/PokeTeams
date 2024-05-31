a46b7a8208ea23aa0cc5fd7c30890f1be4ecb7dc
Sergio <sergiocortmon@gmail.com>
2024-05-31 12:49:04 +0200

    feat(ui): add pagination component

---
+++ ui/src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss'
+})
+export class PaginationComponent 
+{
+  //https://medium.com/@hassaanali.dev/efficient-data-pagination-in-angular-implementing-smooth-and-performant-pagination-for-large-data-637fe994bbbf
+  
+  @Input() currentPage: number = 1;
+  @Input() itemsPerPage!: number;
+  @Input() totalItems!: number;
+  @Output() pageChanged: EventEmitter<number> = new EventEmitter();
+
+  ngOnChanges(changes: SimpleChanges)
+  {
+    console.log(changes)
+  }
+  
+  get totalPages(): number
+  {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  changePage(page: number): void 
+  {
+    if (page >= 1 && page <= this.totalPages) 
+    {
+      this.currentPage = page;
+      this.pageChanged.emit(page);
+      console.log(page)
+    }
+  }
+}
