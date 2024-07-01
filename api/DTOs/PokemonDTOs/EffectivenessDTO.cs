05eea21f391f781128867bfd628e2b7b648d08b3
Sergio <sergiocortmon@gmail.com>
2024-07-01 22:00:59 +0200

    feat(api): change effectiveness to use effectivenessless types

--- api/DTOs/PokemonDTOs/EffectivenessDTO.cs
+++ api/DTOs/PokemonDTOs/EffectivenessDTO.cs
@@ -5,19 +5,19 @@ namespace api.DTOs.PokemonDTOs
     public class EffectivenessDTO
     {
         public List<Tuple<string, double>>? AllValues { get; set; }
-        public List<PokeTypeWithEffectivenessDTO> DoubleSuperEffective { get; set; }
-        public List<PokeTypeWithEffectivenessDTO> SuperEffective { get; set; }
-        public List<PokeTypeWithEffectivenessDTO> NotVeryEffective { get; set; }
-        public List<PokeTypeWithEffectivenessDTO> DoubleNotVeryEffective { get; set; }
-        public List<PokeTypeWithEffectivenessDTO> Inmune { get; set; }
+        public List<PokeTypeDTO> DoubleSuperEffective { get; set; }
+        public List<PokeTypeDTO> SuperEffective { get; set; }
+        public List<PokeTypeDTO> NotVeryEffective { get; set; }
+        public List<PokeTypeDTO> DoubleNotVeryEffective { get; set; }
+        public List<PokeTypeDTO> Inmune { get; set; }
 
         public EffectivenessDTO(List<Tuple<string, double>> allValues)
         {
-            DoubleSuperEffective = new List<PokeTypeWithEffectivenessDTO>();
-            SuperEffective = new List<PokeTypeWithEffectivenessDTO>();
-            NotVeryEffective = new List<PokeTypeWithEffectivenessDTO>();
-            DoubleNotVeryEffective = new List<PokeTypeWithEffectivenessDTO>();
-            Inmune = new List<PokeTypeWithEffectivenessDTO>();
+            DoubleSuperEffective = new List<PokeTypeDTO>();
+            SuperEffective = new List<PokeTypeDTO>();
+            NotVeryEffective = new List<PokeTypeDTO>();
+            DoubleNotVeryEffective = new List<PokeTypeDTO>();
+            Inmune = new List<PokeTypeDTO>();
 
             AllValues = allValues;
             if (AllValues != null)
@@ -27,26 +27,23 @@ namespace api.DTOs.PokemonDTOs
                     switch (value.Item2)
                     {
                         case 4:
-                            DoubleSuperEffective.Add(new PokeTypeWithEffectivenessDTO(value.Item1, value.Item1));
+                            DoubleSuperEffective.Add(new PokeTypeDTO(value.Item1, value.Item1));
                             break;
                         case 2:
-                            SuperEffective.Add(new PokeTypeWithEffectivenessDTO(value.Item1, value.Item1));
+                            SuperEffective.Add(new PokeTypeDTO(value.Item1, value.Item1));
                             break;
                         case 0.5:
-                            NotVeryEffective.Add(new PokeTypeWithEffectivenessDTO(value.Item1, value.Item1));
+                            NotVeryEffective.Add(new PokeTypeDTO(value.Item1, value.Item1));
                             break;
                         case 0.25:
-                            DoubleNotVeryEffective.Add(new PokeTypeWithEffectivenessDTO(value.Item1, value.Item1));
+                            DoubleNotVeryEffective.Add(new PokeTypeDTO(value.Item1, value.Item1));
                             break;
                         case 0:
-                            Inmune.Add(new PokeTypeWithEffectivenessDTO(value.Item1, value.Item1));
+                            Inmune.Add(new PokeTypeDTO(value.Item1, value.Item1));
                             break;
                     }
                 }
             }
-
-
-
         }
     }
 }
