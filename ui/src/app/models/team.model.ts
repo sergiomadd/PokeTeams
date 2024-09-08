9a30547f7120546c9d2e9a4504e046534c9d7325
Sergio <sergiocortmon@gmail.com>
2024-09-08 21:40:18 +0200

    feat(ui): add userPreview model

--- ui/src/app/models/team.model.ts
+++ ui/src/app/models/team.model.ts
@@ -3,13 +3,14 @@ import { Regulation } from "./regulation.model";
 import { Tag } from "./tag.model";
 import { TeamOptions } from "./teamOptions.model";
 import { Tournament } from "./tournament.model";
+import { UserPreview } from "./userPreview.model";
 
 export interface Team
 {
   id: string,
   pokemons: Pokemon[],
   options: TeamOptions,
-  player?: string,
+  player?: UserPreview,
   tournament?: Tournament,
   regulation?: Regulation,
   viewCount: number,
---
+++ ui/src/app/models/userPreview.model.ts
@@ -0,0 +1,5 @@
+export interface UserPreview
+{
+  username: string,
+  picture?: string
+}
