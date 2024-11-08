0d9a6422e9d3f710032c328b4217941215c069a9
Sergio <sergiocortmon@gmail.com>
2024-11-08 13:40:51 +0100

    feat(ui): add load example paste button

--- ui/src/app/features/team-editor/components/input/input.component.ts
+++ ui/src/app/features/team-editor/components/input/input.component.ts
@@ -25,14 +25,102 @@ export class InputComponent
     {
       paste: ["", [Validators.required]]
     });
-  pasteHolder: string;
+  pasteHolder: string = "";
+
   team: Team = <Team>{};
   selectedPokemonIndex: number = 0;
   tabs: boolean[] = [true, false];
 
   constructor()
   {
-    this.pasteHolder = 
+    
+  }
+
+  ngOnInit() 
+  {
+    this.teamEditorService.selectedTeam$.subscribe((value) => 
+    {
+      this.team = value;
+    })
+    this.pasteBoxForm.controls.paste.valueChanges.subscribe(value => 
+      {
+        if(value && this.pasteBoxFormSubmitted)
+        {
+          this.pasteBoxFormSubmitted = false;
+        }
+      })
+  }
+
+  async load()
+  {
+    //const nowAll = new Date().getTime();
+    this.pasteBoxFormSubmitted = true;
+    if(this.pasteBoxForm.valid)
+    {
+      let formData = this.pasteBoxForm.controls.paste.value ?? "";
+      console.log("Submitting: ", formData)
+      let data = this.parser.parsePaste(formData);
+      console.log("Parsed data: ", data)
+      if(data.pokemons && data.pokemons.length > 0)
+      {
+        this.teamEditorService.updatePokemons([]);
+        for (const pokePaste of data.pokemons)
+        {
+          const pokemon: Pokemon = await this.pokemonService.buildPokemon(pokePaste);
+          this.teamEditorService.addPokemon(pokemon);
+        };
+      }
+    }
+    //console.log("Time to generate pokemons: ", new Date().getTime() - nowAll);
+  }
+
+  addEmptyPokemon()
+  {
+    this.teamEditorService.addEmptyPokemon();
+    this.selectedPokemonIndex = this.team.pokemons.length - 1;
+  }
+
+  deletePokemon(index: number)
+  {
+    this.teamEditorService.deletePokemon(index);
+  }
+
+  selectTab(index)
+  {
+    for(let i=0;i<this.tabs.length;i++)
+    {
+      this.tabs[i] = false;
+    }
+    this.tabs[index] = true;
+  }
+
+  selectPokemon(index: number)
+  {
+    if(index != this.selectedPokemonIndex)
+    {
+      this.selectedPokemonIndex = index;
+    }
+  }
+
+  isInvalid(key: string) : boolean
+  {
+    var control = this.pasteBoxForm.get(key);
+    let invalid = (control?.errors
+      && this.pasteBoxFormSubmitted)
+      ?? false;
+    return invalid;
+  }
+
+  getError(key: string) : string
+  {
+    let control: AbstractControl | null =  this.pasteBoxForm.get(key);
+    return this.util.getAuthFormError(control);
+  }
+
+  loadExample()
+  {
+
+    const testPaste = 
     `
     monito (Garchomp) @ Soul Dew  
     Ability: Technician  
@@ -107,82 +195,78 @@ export class InputComponent
     - Sunny Day  
     - Sunny Day
     `
+
+    const propaste = 
+    `
+Zacian @ Rusted Sword  
+Ability: Intrepid Sword  
+Level: 50  
+Shiny: Yes  
+EVs: 236 HP / 76 Atk / 4 Def / 4 SpD / 188 Spe  
+Adamant Nature  
+- Behemoth Blade  
+- Play Rough  
+- Sacred Sword  
+- Protect  
+
+Kyogre @ Mystic Water  
+Ability: Drizzle  
+Level: 50  
+EVs: 116 HP / 28 Def / 108 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Water Spout  
+- Origin Pulse  
+- Ice Beam  
+- Protect  
+
+Kartana @ Assault Vest  
+Ability: Beast Boost  
+Level: 50  
+Shiny: Yes  
+EVs: 4 HP / 4 Atk / 4 Def / 244 SpD / 252 Spe  
+Jolly Nature  
+- Leaf Blade  
+- Smart Strike  
+- Sacred Sword  
+- Aerial Ace  
+
+Shedinja @ Focus Sash  
+Ability: Wonder Guard  
+Level: 50  
+EVs: 252 Atk / 252 Spe  
+Lonely Nature  
+IVs: 0 Def / 0 SpD  
+- Poltergeist  
+- Shadow Sneak  
+- Endure  
+- Ally Switch  
+
+Incineroar @ Shuca Berry  
+Ability: Intimidate  
+Level: 50  
+Shiny: Yes  
+EVs: 252 HP / 4 Atk / 108 Def / 132 SpD / 12 Spe  
+Impish Nature  
+IVs: 0 SpA  
+- Flare Blitz  
+- Darkest Lariat  
+- Parting Shot  
+- Fake Out  
+
+Thundurus @ Sitrus Berry  
+Ability: Prankster  
+Level: 50  
+EVs: 236 HP / 140 Def / 4 SpA / 76 SpD / 52 Spe  
+Calm Nature  
+IVs: 0 Atk  
+- Thunderbolt  
+- Taunt  
+- Eerie Impulse  
+- Thunder Wave  
+`
+    this.pasteHolder = propaste;
+    this.pasteBoxForm.controls.paste.setValue(propaste);
   }
 
-  ngOnInit() 
-  {
-    this.pasteBoxForm.controls.paste.setValue(this.pasteHolder);
-    this.teamEditorService.selectedTeam$.subscribe((value) => 
-    {
-      this.team = value;
-    })
-  }
-
-  async load()
-  {
-    //const nowAll = new Date().getTime();
-    this.pasteBoxFormSubmitted = true;
-    console.log(this.isInvalid('paste'))
-    if(this.pasteBoxForm.valid)
-    {
-      let formData = this.pasteBoxForm.controls.paste.value ?? "";
-      console.log("Submitting: ", formData)
-      let data = this.parser.parsePaste(formData);
-      console.log("Parsed data: ", data)
-      if(data.pokemons && data.pokemons.length > 0)
-      {
-        this.teamEditorService.updatePokemons([]);
-        for (const pokePaste of data.pokemons)
-        {
-          const pokemon: Pokemon = await this.pokemonService.buildPokemon(pokePaste);
-          this.teamEditorService.addPokemon(pokemon);
-        };
-      }
-    }
-    //console.log("Time to generate pokemons: ", new Date().getTime() - nowAll);
-  }
-
-  addEmptyPokemon()
-  {
-    this.teamEditorService.addEmptyPokemon();
-    this.selectedPokemonIndex = this.team.pokemons.length - 1;
-  }
-
-  deletePokemon(index: number)
-  {
-    this.teamEditorService.deletePokemon(index);
-  }
-
-  selectTab(index)
-  {
-    for(let i=0;i<this.tabs.length;i++)
-    {
-      this.tabs[i] = false;
-    }
-    this.tabs[index] = true;
-  }
-
-  selectPokemon(index: number)
-  {
-    if(index != this.selectedPokemonIndex)
-    {
-      this.selectedPokemonIndex = index;
-    }
-  }
-
-  isInvalid(key: string) : boolean
-  {
-    var control = this.pasteBoxForm.get(key);
-    let invalid = (control?.errors
-      && (control?.dirty || control?.touched
-        || this.pasteBoxFormSubmitted))
-      ?? false;
-    return invalid;
-  }
-
-  getError(key: string) : string
-  {
-    let control: AbstractControl | null =  this.pasteBoxForm.get(key);
-    return this.util.getAuthFormError(control);
-  }
 }
