df07d43282f165948af55274ff553e496b04f7fb
Sergio <sergiocortmon@gmail.com>
2024-11-13 20:33:33 +0100

    feat(ui): remove teams & teamkeys from user model

--- ui/src/app/features/user/models/user.model.ts
+++ ui/src/app/features/user/models/user.model.ts
@@ -1,13 +1,10 @@
 import { Country } from "../../../models/DTOs/country.dto";
-import { TeamPreview } from "../../team/models/teamPreview.model";
 
 export interface User
 {
   name: string,
   username: string,
   picture?: string,
-  teams?: TeamPreview[],
-  teamKeys?: string[],
   country?: Country,
   visibility: boolean,
   //logged
