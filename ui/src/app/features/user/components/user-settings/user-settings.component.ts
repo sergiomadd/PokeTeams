93aa79056f2c276ed61dc6a4e093cb7b87fd59d9
Sergio <sergiocortmon@gmail.com>
2024-11-14 12:54:16 +0100

    feat(ui): replace user options with user settings & rework ui

--- ui/src/app/features/user/components/user-settings/user-settings.component.ts
+++ ui/src/app/features/user/components/user-settings/user-settings.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { authActions } from 'src/app/auth/store/auth.actions';
 import { selectValidationErrors } from 'src/app/auth/store/auth.selectors';
-import { User } from 'src/app/features/user/models/user.model';
-import { UserService } from 'src/app/features/user/services/user.service';
 import { Country } from 'src/app/models/DTOs/country.dto';
 import { UserUpdateDTO } from 'src/app/models/DTOs/userUpdate.dto';
 import { UtilService } from 'src/app/shared/services/util.service';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
-  selector: 'app-user-options',
-  templateUrl: './user-options.component.html',
-  styleUrls: ['./user-options.component.scss']
+  selector: 'app-user-settings',
+  templateUrl: './user-settings.component.html',
+  styleUrl: './user-settings.component.scss'
 })
-export class UserOptionsComponent 
+export class UserSettingsComponent 
 {
   @Input() loggedUser?: User;
   deleteDialog: boolean = false;
