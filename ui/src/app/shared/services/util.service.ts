16c5cf190ecae9edc0c9c15df9ccb62c08af385d
Sergio <sergiocortmon@gmail.com>
2024-11-29 12:24:07 +0100

    feat(ui): add view count formatting

--- ui/src/app/shared/services/util.service.ts
+++ ui/src/app/shared/services/util.service.ts
@@ -299,4 +299,20 @@ export class UtilService
   {
     return typeof +value !== "number" || isNaN(+value)
   }
+
+  formatCount(count: number)
+  {
+    let formated: string = count.toString();
+    let rounded: number = count;
+    let letter: string = "";
+    const decimals: number = 2;
+    if(count >= 1e3) 
+    {
+      letter = 'K'
+      rounded = count / 1e3
+      formated = rounded.toFixed(decimals)
+    }
+    formated = formated + letter;
+    return formated;
+  }
 }
