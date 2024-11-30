f914e3ba17eae115929880e3a2a3adf05aab0d91
Sergio <sergiocortmon@gmail.com>
2024-11-30 12:55:59 +0100

    feat(api): add stat identifier converter

--- api/DTOs/PokemonDTOs/StatDTO.cs
+++ api/DTOs/PokemonDTOs/StatDTO.cs
@@ -7,9 +7,19 @@
         public int? Value { get; set; }
         public StatDTO(string? identifier, string? name, int? value)
         {
-            Identifier = identifier;
+            Identifier = identifier != null ? StatIdentifierConverter[identifier] : identifier;
             Name = name;
             Value = value;
         }
+
+        public Dictionary<string, string> StatIdentifierConverter = new Dictionary<string, string>
+        {
+            { "hp", "hp" },
+            { "attack", "atk" },
+            { "defense", "def" },
+            { "special-attack", "spa" },
+            { "special-defense", "spd" },
+            { "speed", "spe" },
+        };
     }
 }
