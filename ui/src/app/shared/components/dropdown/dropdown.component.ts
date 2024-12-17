921e84ae70b4df8f2e00ef317314fd144f824614
Sergio <sergiocortmon@gmail.com>
2024-12-17 11:59:44 +0100

    feat(ui): rework dropdown component

--- ui/src/app/shared/components/dropdown/dropdown.component.ts
+++ ui/src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,13 +1,5 @@
-import { Component, EventEmitter, Input, Output, SimpleChange, SimpleChanges, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { NgOptimizedImage } from '@angular/common'
-
-export interface DropdownOption
-{
-  name: string,
-  code?: string,
-  icon?: string
-}
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Tag } from 'src/app/features/team/models/tag.model';
 
 
 @Component({
@@ -17,69 +9,16 @@ export interface DropdownOption
 })
 export class DropdownComponent 
 {
-  formBuilder = inject(FormBuilder);
-
-  @Input() options?: DropdownOption[];
-  @Input() selectedOption?: DropdownOption;
+  @Input() options?: Tag[];
+  @Input() selectedOption?: Tag;
+  @Input() onlyIcon?: boolean;
   @Input() disable?: boolean;
   
-  @Output() selectEvent = new EventEmitter<DropdownOption>();
+  @Output() selectEvent = new EventEmitter<Tag>();
 
   showOptions?: boolean = false;
-  showInput?: boolean = false;
-  filteredOptions?: DropdownOption[] = [];
 
-  form = this.formBuilder.group(
-    {
-      value: [this.selectedOption?.name],
-    });
-
-  ngOnInit()
-  {
-    this.filteredOptions = this.options;
-    this.form.controls.value.valueChanges.subscribe((value) => 
-    {
-      if(value && value != null)
-      {
-        this.filteredOptions = this.options?.filter(o => o.name.toLocaleLowerCase().includes(value));
-        if(!this.showOptions) { this.showOptions = !this.showOptions; }
-      }
-      else
-      {
-        this.filteredOptions = this.options;
-      }
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges)
-  {
-    if(changes['options'])
-    {
-      this.filteredOptions = this.options;
-    }
-  }
-
-  onFocus()
-  {
-    if(!this.showOptions) { this.showOptions = !this.showOptions; }
-  }
-
-  onBlur()
-  {
-    if(this.showOptions) { this.showOptions = !this.showOptions; }
-  }
-
-  clearForm()
-  {
-    this.form.controls.value.setValue('');
-  }
-
-  onClickBody()
-  {
-    this.showInput = !this.showInput;
-  }
-
-  onClickArrow()
+  toggleOptions()
   {
     this.showOptions = !this.showOptions;
   }
@@ -87,7 +26,6 @@ export class DropdownComponent
   onSelect(option)
   {
     this.selectEvent.emit(option);
-    this.onClickArrow();
-    this.form.controls.value.setValue('');
+    this.toggleOptions();
   }
 }
