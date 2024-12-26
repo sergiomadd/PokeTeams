c34073100ec8e4aea915ca059a69c95402bc5fc2
Sergio <sergiocortmon@gmail.com>
2024-12-26 14:00:26 +0100

    feat(ui): add db languages to lang enum

--- ui/src/app/core/config/models/lang.enum.ts
+++ ui/src/app/core/config/models/lang.enum.ts
@@ -1,9 +1,32 @@
 export enum Lang
 {
-  en = "en",
-  es = "es"
+  jp = "jp",
+  ko = "ko",
+  zh = "zh",
+  fr = "fr",
+  de = "de",
+  es = "es",
+  it = "it",
+  en = "en"
 }
 
 export const langs: string[] = [Lang.en, Lang.es]
+//export const langs: string[] = [Lang.en, Lang.es, Lang.jp, Lang.it, Lang.de, Lang.fr, Lang.zh, Lang.ko]
 
 export const flags = ["UK", "ES"]
+
+/*
+1  ja = japanese,
+2  ja = romanji //not used
+3  ko = korean
+4  zh = chinese
+5  fr = french
+6  de = deutch
+7  es = spanish
+8  it = italian
+9  en = english   
+10 cs =  // not used
+11 ja = ??
+12 zh = ??
+13 pt = portuguese //not used
+*/
