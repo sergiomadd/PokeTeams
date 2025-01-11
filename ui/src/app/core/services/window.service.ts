94651912e9d75d5d5d909e8030ffd8cf8969d34b
Sergio <sergiocortmon@gmail.com>
2025-01-11 01:08:40 +0100

    feat(ui): add minimobile breakpoint & implement team & adjust mobile breakpoint value

--- ui/src/app/core/services/window.service.ts
+++ ui/src/app/core/services/window.service.ts
@@ -1,4 +1,4 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,17 +8,23 @@ export class WindowService
 {
   responsive = inject(BreakpointObserver);
 
-  isPhone: boolean = false;
+  isMobile: boolean = false; //600px
+  isMiniMobile: boolean = false;
+
+  mobileSize: string = '(max-width: 768px)';
+  miniMobileSize: string = '(max-width: 480px)';
 
   constructor() 
   {
-    this.responsive.observe([Breakpoints.Handset])
+    this.responsive.observe([this.mobileSize, this.miniMobileSize])
     .subscribe(result => 
     {
       console.log(result)
+      this.isMiniMobile = false;
       if(result.matches)
       {
-        this.isPhone = true;
+        this.isMobile = result.breakpoints[this.mobileSize];
+        this.isMiniMobile = result.breakpoints[this.miniMobileSize];
       }
     });
   }
