ad40d02ab148b86a9fcf3d676b450255b42f55e6
Sergio <sergiocortmon@gmail.com>
2025-01-13 01:29:04 +0100

    feat(ui): add mobile ui css to not found component

--- ui/src/app/shared/components/not-found/not-found.component.ts
+++ ui/src/app/shared/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { WindowService } from 'src/app/core/layout/mobile/window.service';
 
 @Component({
   selector: 'app-not-found',
@@ -7,5 +8,7 @@ import { Component, Input } from '@angular/core';
 })
 export class NotFoundComponent 
 {
+  window = inject(WindowService);
+
   @Input() resourceName?: string;
 }
