89a8daf8d2f85abbf05af387c223cec0c37d94e0
Sergio <sergiocortmon@gmail.com>
2025-03-28 22:14:46 +0100

    feat(api): add testing data and getters

---
+++ api.Test/Data/ExpectedResults.cs
@@ -0,0 +1,105 @@
+﻿using api.DTOs;
+using api.DTOs.PokemonDTOs;
+using api.Models;
+using api.Util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Test.Data
+{
+    public static class ExpectedResults
+    {
+        public static PokedexExpectedResults? TryGetPokedexExpectedResults()
+        {
+            try
+            {
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "pokedexCases.json");
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<PokedexExpectedResults>(json);
+            }
+            catch (Exception e)
+            {
+                Printer.Log("Error loading test expectedResults.json", e);
+            }
+            return null;
+        }
+
+        public static ItemDTO? GetTestItem(int langId, PokedexExpectedResults expected)
+        {
+            switch (langId)
+            {
+                case (int)Lang.es:
+                    return expected.es.itemCases.DTO1.result.ToObject<ItemDTO>();
+                default:
+                    return expected.en.itemCases.DTO1.result.ToObject<ItemDTO>();
+            }
+        }
+
+        public static List<QueryResultDTO>? GetTestItemQuery(int langId, PokedexExpectedResults expected)
+        {
+            switch (langId)
+            {
+                case (int)Lang.es:
+                    return expected.es.itemCases.query1.queryResult.ToObject<List<QueryResultDTO>>();
+                default:
+                    return expected.en.itemCases.query1.queryResult.ToObject<List<QueryResultDTO>>();
+            }
+        }
+
+        public static AbilityDTO? GetTestAbility(int langId, PokedexExpectedResults expected)
+        {
+            try
+            {
+                switch (langId)
+                {
+                    case (int)Lang.es:
+                        return expected.es.abilityCases.DTO1.result.ToObject<AbilityDTO>();
+                    default:
+                        return expected.en.abilityCases.DTO1.result.ToObject<AbilityDTO>();
+                }
+            }
+            catch (Exception e)
+            {
+                Printer.Log("Exeption in GetTestAbility: ", e);
+            }
+            return null;
+        }
+
+        public static MoveDTO? GetTestMove(int langId, PokedexExpectedResults expected)
+        {
+            switch (langId)
+            {
+                case (int)Lang.es:
+                    return expected.es.moveCases.DTO1.result.ToObject<MoveDTO>();
+                default:
+                    return expected.en.moveCases.DTO1.result.ToObject<MoveDTO>();
+            }
+        }
+
+        public static NatureDTO? GetTestNature(int langId, PokedexExpectedResults expected)
+        {
+            switch (langId)
+            {
+                case (int)Lang.es:
+                    return expected.es.natureCases.DTO1.result.ToObject<NatureDTO>();
+                default:
+                    return expected.en.natureCases.DTO1.result.ToObject<NatureDTO>();
+            }
+        }
+
+        public static PokeTypeDTO? GetTestType(int langId, PokedexExpectedResults expected)
+        {
+            switch (langId)
+            {
+                case (int)Lang.es:
+                    return expected.es.typeCases.DTO1.result.ToObject<PokeTypeDTO>();
+                default:
+                    return expected.en.typeCases.DTO1.result.ToObject<PokeTypeDTO>();
+            }
+        }
+    }
+}
---
+++ api.Test/Data/PokedexExpectedResults.cs
@@ -0,0 +1,45 @@
+﻿using api.DTOs;
+using api.DTOs.PokemonDTOs;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Test.Data
+{
+    public class PokedexExpectedResults
+    {
+        public LocalizedCase en { get; set; }
+        public LocalizedCase es { get; set; }
+
+    }
+
+    public class LocalizedCase
+    {
+        public CaseCategory itemCases { get; set; }
+        public CaseCategory abilityCases { get; set; }
+        public CaseCategory moveCases { get; set; }
+        public CaseCategory natureCases { get; set; }
+        public CaseCategory typeCases { get; set; }
+    }
+
+    public class CaseCategory
+    {
+        public Case DTO1 { get; set; }
+        public QueryCase query1 { get; set; }
+    }
+
+    public class Case
+    {
+        public JValue identifier { get; set; }
+        public JObject result { get; set; }
+    }
+
+    public class QueryCase
+    {
+        public JValue identifier { get; set; }
+        public JArray queryResult { get; set; }
+    }
+}
