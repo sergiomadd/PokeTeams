acf60a67e34ab7e0156b77b56b3d1af16d4dc47e
Sergio <sergiocortmon@gmail.com>
2025-04-16 20:52:17 +0200

    feat(ui): improve user visibility switch

--- ui/src/app/shared/components/dumb/switch/switch.component.ts
+++ ui/src/app/shared/components/dumb/switch/switch.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-switch',
@@ -10,6 +10,8 @@ export class SwitchComponent
   @Input() inputState?: boolean;
   @Input() leftText?: string;
   @Input() rightText?: string;
+  @Input() leftSVG?: TemplateRef<any>;
+  @Input() rightSVG?: TemplateRef<any>;
   @Output() checkEvent = new EventEmitter<boolean>();
 
   state: boolean = false;
@@ -19,9 +21,16 @@ export class SwitchComponent
     if(this.inputState) {this.state = this.inputState}
   }
 
+  ngOnChanges(changes: SimpleChanges) 
+  {
+    if(changes["inputState"]) 
+    {
+      this.state = changes["inputState"].currentValue
+    }
+  }
+
   onClick()
   {
-    this.state = !this.state;
-    this.checkEvent.emit(this.state);
+    this.checkEvent.emit(!this.state);
   }
 }
