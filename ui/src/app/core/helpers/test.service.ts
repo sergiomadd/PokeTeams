6e0c648b800fda7b215015dadae775476b90a264
Sergio <sergiocortmon@gmail.com>
2025-06-02 14:21:34 +0200

    feat(ui): add testdata service and remove hardcoded pastes from parte input

---
+++ ui/src/app/core/helpers/test.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService 
+{
+  private apiUrl = environment.apiUrl + 'testdata/';
+
+  constructor(private http: HttpClient) { }
+
+  getTestForms(): Observable<string>
+  {
+    let url = this.apiUrl + "forms";
+    return this.http.get<string>(url, {withCredentials: true});
+  }
+
+  getTestPaste(key: string): Observable<string>
+  {
+    let url = this.apiUrl + "paste/" + key;
+    return this.http.get<string>(url, {withCredentials: true});
+  }
+}
--- ui/src/app/shared/components/team/paste-input/paste-input.component.ts
+++ ui/src/app/shared/components/team/paste-input/paste-input.component.ts
@@ -3,6 +3,7 @@ import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ParserService } from 'src/app/core/helpers/parser.service';
+import { TestService } from 'src/app/core/helpers/test.service';
 import { UtilService } from 'src/app/core/helpers/util.service';
 import { WindowService } from 'src/app/core/helpers/window.service';
 import { Team } from 'src/app/core/models/team/team.model';
@@ -24,6 +25,7 @@ export class PasteInputComponent
   util = inject(UtilService);
   window = inject(WindowService);
   store = inject(Store);
+  testService = inject(TestService);
 
   selectedLang$: Observable<string> = this.store.select(selectLang);
 
@@ -116,238 +118,18 @@ export class PasteInputComponent
 
   loadExample()
   {
-
-    const testPaste = 
-    `
-    monito (Garchomp) @ Soul Dew  
-    Ability: Technician  
-    Level: 78  
-    Shiny: Yes  
-    Tera Type: Dark  
-    EVs: 74 HP / 190 Atk / 91 Def / 48 SpA / 84 SpD / 23 Spe  
-    Adamant Nature  
-    IVs: 24 HP / 12 Atk / 30 Def / 16 SpA / 23 SpD / 5 Spe  
-    - Scary Face  
-    - Smack Down  
-    - Sunny Day  
-    - Sunny Day
-
-    Pickle (Mamoswine) (F) @ Never-Melt Ice
-    Ability: Thick Fat
-    Shiny: Yes
-    EVs: 252 Atk / 4 SpD / 252 Spe
-    Jolly Nature
-    - Ice Shard
-    - Earthquake
-    - Icicle Crash
-    - Knock Off
-
-    Kyogre @ Mystic Water
-    Ability: Drizzle
-    Level: 100
-    EVs: 116 HP / 28 Def / 108 SpA / 4 SpD / 252 Spe
-    Timid Nature
-    IVs: 0 Atk
-    - Water Spout
-    - Origin Pulse
-    - Ice Beam
-    - Protect
-
-    monito (Eevee) @ Soul Dew  
-    Ability: Technician  
-    Level: 49  
-    Shiny: Yes  
-    Tera Type: Dark  
-    EVs: 25 HP / 25 Atk / 25 Def / 25 SpA / 25 SpD / 25 Spe  
-    Bashful Nature  
-    IVs: 7 Atk / 6 Def / 9 SpD / 4 Spe 
-    - Scary Face  
-    - Smack Down  
-    - Sunny Day  
-    - Sunny Day
-
-    monito (Charmander) @ Soul Dew  
-    Ability: Technician  
-    Level: 49  
-    Shiny: Yes  
-    Tera Type: Dark  
-    EVs: 25 HP / 25 Atk / 25 Def / 25 SpA / 25 SpD / 25 Spe  
-    Bashful Nature  
-    IVs: 7 Atk / 6 Def / 9 SpD / 4 Spe 
-    - Scary Face  
-    - Smack Down  
-    - Sunny Day  
-    - Sunny Day
-
-    monito (Metagross) @ Soul Dew  
-    Ability: Technician  
-    Level: 49  
-    Shiny: Yes  
-    Tera Type: Dark  
-    EVs: 25 HP / 25 Atk / 25 Def / 25 SpA / 25 SpD / 25 Spe  
-    Bashful Nature  
-    IVs: 7 Atk / 6 Def / 9 SpD / 4 Spe 
-    - Scary Face  
-    - Smack Down  
-    - Sunny Day  
-    - Sunny Day
-    `
-
-    const one = 
-    `
-Zacian @ Banda Focus 
-Ability: Intrepid Sword  
-Level: 50  
-Shiny: Yes  
-EVs: 236 HP / 76 Atk / 4 Def / 4 SpD / 188 Spe  
-Adamant Nature  
-- Behemoth Blade  
-- Play Rough  
-- Sacred Sword  
-- Protect  `
-
-    const propaste = 
-    `
-Zacian @ Banda Focus 
-Ability: Intrepid Sword  
-Level: 50  
-Shiny: Yes  
-EVs: 236 HP / 76 Atk / 4 Def / 4 SpD / 188 Spe  
-Adamant Nature  
-- Behemoth Blade  
-- Play Rough  
-- Sacred Sword  
-- Protect  
-
-Kyogre @ Mystic Water  
-Ability: Drizzle  
-Level: 50  
-EVs: 116 HP / 28 Def / 108 SpA / 4 SpD / 252 Spe  
-Timid Nature  
-IVs: 0 Atk  
-- Water Spout  
-- Origin Pulse  
-- Ice Beam  
-- Protect  
-
-Kartana @ Assault Vest  
-Ability: Beast Boost  
-Level: 50  
-Shiny: Yes  
-EVs: 4 HP / 4 Atk / 4 Def / 244 SpD / 252 Spe  
-Jolly Nature  
-- Leaf Blade  
-- Smart Strike  
-- Sacred Sword  
-- Aerial Ace  
-
-Shedinja @ Focus Sash  
-Ability: Wonder Guard  
-Level: 50  
-EVs: 252 Atk / 252 Spe  
-Lonely Nature  
-IVs: 0 Def / 0 SpD  
-- Poltergeist  
-- Shadow Sneak  
-- Endure  
-- Ally Switch  
-
-Incineroar @ Shuca Berry  
-Ability: Intimidate  
-Level: 50  
-Shiny: Yes  
-EVs: 252 HP / 4 Atk / 108 Def / 132 SpD / 12 Spe  
-Impish Nature  
-IVs: 0 SpA  
-- Flare Blitz  
-- Darkest Lariat  
-- Parting Shot  
-- Fake Out  
-
-Thundurus @ Sitrus Berry  
-Ability: Prankster  
-Level: 50  
-EVs: 236 HP / 140 Def / 4 SpA / 76 SpD / 52 Spe  
-Calm Nature  
-IVs: 0 Atk  
-- Thunderbolt  
-- Taunt  
-- Eerie Impulse  
-- Thunder Wave  
-`
-
-const test4 = 
-`
-Miraidon @ Choice Scarf  
-Ability: Hadron Engine  
-Level: 50  
-Tera Type: Electric  
-EVs: 4 Def / 252 SpA / 252 Spe  
-Modest Nature  
-- Electro Drift  
-- Draco Meteor  
-- Volt Switch  
-- Snarl  
-
-Calyrex-Ice @ Clear Amulet  
-Ability: As One (Glastrier)  
-Level: 50  
-Tera Type: Water  
-EVs: 252 HP / 116 Atk / 4 Def / 84 SpD / 52 Spe  
-Adamant Nature  
-- Glacial Lance  
-- High Horsepower  
-- Trick Room  
-- Protect  
-
-Iron Valiant @ Booster Energy  
-Ability: Quark Drive  
-Level: 50  
-Tera Type: Ghost  
-EVs: 252 HP / 4 Atk / 68 Def / 44 SpD / 140 Spe  
-Jolly Nature  
-- Spirit Break  
-- Encore  
-- Disable  
-- Protect  
-
-Volcarona @ Rocky Helmet  
-Ability: Flame Body  
-Level: 50  
-Tera Type: Water  
-EVs: 252 HP / 220 Def / 4 SpA / 4 SpD / 28 Spe  
-Bold Nature  
-IVs: 0 Atk  
-- Fiery Dance  
-- Rage Powder  
-- Will-O-Wisp  
-- Tailwind  
-
-Urshifu-Rapid-Strike @ Focus Sash  
-Ability: Unseen Fist  
-Level: 50  
-Tera Type: Stellar  
-EVs: 252 Atk / 4 SpD / 252 Spe  
-Adamant Nature  
-- Surging Strikes  
-- Close Combat  
-- Aqua Jet  
-- Detect  
-
-Farigiraf @ Electric Seed  
-Ability: Armor Tail  
-Level: 50  
-Tera Type: Dark  
-EVs: 236 HP / 108 Def / 4 SpA / 148 SpD / 12 Spe  
-Bold Nature  
-IVs: 0 Atk  
-- Psychic  
-- Foul Play  
-- Helping Hand  
-- Trick Room  
-
-`
-    this.pasteHolder = propaste;
-    this.pasteBoxForm.controls.paste.setValue(this.pasteHolder);
+    /*
+    this.testService.getTestForms().subscribe(value => 
+      {
+        this.pasteHolder = value;
+        this.pasteBoxForm.controls.paste.setValue(this.pasteHolder);
+      })
+    */
+    this.testService.getTestPaste("test6").subscribe(value => 
+    {
+      this.pasteHolder = value;
+      this.pasteBoxForm.controls.paste.setValue(this.pasteHolder);
+    })
+    
   }
 }
