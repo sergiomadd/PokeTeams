021863adf14c2b21fa2525b43c6fae9414985359
Sergio <sergiocortmon@gmail.com>
2025-06-07 11:30:19 +0200

    fix(ui): small ui adjustments

--- ui/src/app/shared/components/dumb/no-translation/no-translation.component.ts
+++ ui/src/app/shared/components/dumb/no-translation/no-translation.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-no-translation',
   templateUrl: './no-translation.component.html',
   styleUrl: './no-translation.component.scss'
 })
-export class NoTranslationComponent {
-
+export class NoTranslationComponent 
+{
+  @Input() noTooltip?: boolean = false;
 }
