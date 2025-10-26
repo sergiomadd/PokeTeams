f22f58469881d7e94aa4135f38bca22953168b53
Sergio Cortés <sergiocortmon@gmail.com>
2025-10-26 10:32:11 +0100

    update(ui): migrate app boostraping

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,14 +1,5 @@
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { CoreModule } from './app/core/core.module';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, CoreModule),
-        provideAnimations()
-    ]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
