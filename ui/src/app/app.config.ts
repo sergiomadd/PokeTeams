c104d095dc6273f2d3ab82b5c2b12a38c3269aca
Sergio Cortés <sergiocortmon@gmail.com>
2025-10-28 13:50:51 +0100

    fix(ui): downgrade social login library until fixed and move directly to config

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,7 +1,9 @@
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from "@abacritt/angularx-social-login";
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from "@angular/core";
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withViewTransitions } from "@angular/router";
+import { environment } from "../environments/environment";
 import { routes } from "./app.routes";
 import { provideCore } from "./core/core.providers";
 
@@ -10,6 +12,21 @@ export const appConfig: ApplicationConfig =
   providers: 
   [
     provideCore(),
+    importProvidersFrom(SocialLoginModule),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        lang: 'en',
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(environment.googleId),
+          },
+        ],
+        onError: (err: unknown) => console.error(err),
+      } as SocialAuthServiceConfig,
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
