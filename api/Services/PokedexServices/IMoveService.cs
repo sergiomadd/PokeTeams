35b3ce0306d49dff569cb6df7876e24f2127d402
Sergio Cortés <sergiocortmon@gmail.com>
2026-09-22 12:22:15 +0200

    feat(api): add getMoveByIdentifier and getTypeById

--- api/Services/PokedexServices/IMoveService.cs
+++ api/Services/PokedexServices/IMoveService.cs
@@ -6,8 +6,10 @@ namespace api.Services.PokedexServices
     public interface IMoveService
     {
         public Task<MoveDTO?> GetMoveByIdentifier(string identifier, int langId);
+        public Task<Dictionary<string, MoveDTO>> GetMovesByIdentifiers(List<string> identifiers, int langId);
         public Task<MoveDTO?> GetMoveByName(string name, int langId);
         public Task<MovePreviewDTO?> GetMovePreviewByIdentifier(string identifier, int langId);
+        public Task<Dictionary<string, MovePreviewDTO>> GetMovePreviewsByIdentifiers(List<string> identifiers, int langId);
         public Task<List<QueryResultDTO>> QueryMovesByName(string key, int langId);
     }
 }
